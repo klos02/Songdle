f43a5ad8671d8c9f982e992b1b47cf931328be5e
klos02 <bartoszklosinski02@gmail.com>
2025-07-31 19:57:43 +0200

    add dbcontext

---
+++ Songdle.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using Songdle.Domain.Entities;
+
+namespace Songdle.Infrastructure.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<Song> Songs { get; set; }
+}
